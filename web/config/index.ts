8604d67945e48985cfcf5a607db011cb1998bf59
jescudero <jescudero@itos.es>
2024-11-19 18:07:31 +0100

    refactor: remove unused CSS styles, TypeScript models, and components across the application

--- web/config/index.ts
+++ web/config/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-mutable-exports */
 import { InputVarType } from '@/app/components/workflow/types'
 import { AgentStrategy } from '@/types/app'
-import { PromptRole } from '@/models/debug'
 
 export let apiPrefix = ''
 export let publicApiPrefix = ''
@@ -34,66 +33,6 @@ const EDITION = process.env.NEXT_PUBLIC_EDITION || globalThis.document?.body?.ge
 export const IS_CE_EDITION = EDITION === 'SELF_HOSTED'
 
 export const SUPPORT_MAIL_LOGIN = !!(process.env.NEXT_PUBLIC_SUPPORT_MAIL_LOGIN || globalThis.document?.body?.getAttribute('data-public-support-mail-login'))
-
-export const TONE_LIST = [
-  {
-    id: 1,
-    name: 'Creative',
-    config: {
-      temperature: 0.8,
-      top_p: 0.9,
-      presence_penalty: 0.1,
-      frequency_penalty: 0.1,
-    },
-  },
-  {
-    id: 2,
-    name: 'Balanced',
-    config: {
-      temperature: 0.5,
-      top_p: 0.85,
-      presence_penalty: 0.2,
-      frequency_penalty: 0.3,
-    },
-  },
-  {
-    id: 3,
-    name: 'Precise',
-    config: {
-      temperature: 0.2,
-      top_p: 0.75,
-      presence_penalty: 0.5,
-      frequency_penalty: 0.5,
-    },
-  },
-  {
-    id: 4,
-    name: 'Custom',
-  },
-]
-
-export const DEFAULT_CHAT_PROMPT_CONFIG = {
-  prompt: [
-    {
-      role: PromptRole.system,
-      text: '',
-    },
-  ],
-}
-
-export const DEFAULT_COMPLETION_PROMPT_CONFIG = {
-  prompt: {
-    text: '',
-  },
-  conversation_histories_role: {
-    user_prefix: '',
-    assistant_prefix: '',
-  },
-}
-
-export const getMaxToken = (modelId: string) => {
-  return (modelId === 'gpt-4' || modelId === 'gpt-3.5-turbo-16k') ? 8000 : 4000
-}
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
@@ -155,93 +94,6 @@ export const DEFAULT_AGENT_SETTING = {
   max_iteration: 5,
   strategy: AgentStrategy.functionCall,
   tools: [],
-}
-
-export const DEFAULT_AGENT_PROMPT = {
-  chat: `Respond to the human as helpfully and accurately as possible.
-
-  {{instruction}}
-
-  You have access to the following tools:
-
-  {{tools}}
-
-  Use a json blob to specify a tool by providing an {{TOOL_NAME_KEY}} key (tool name) and an {{ACTION_INPUT_KEY}} key (tool input).
-  Valid "{{TOOL_NAME_KEY}}" values: "Final Answer" or {{tool_names}}
-
-  Provide only ONE action per $JSON_BLOB, as shown:
-
-  \`\`\`
-  {
-    "{{TOOL_NAME_KEY}}": $TOOL_NAME,
-    "{{ACTION_INPUT_KEY}}": $ACTION_INPUT
-  }
-  \`\`\`
-
-  Follow this format:
-
-  Question: input question to answer
-  Thought: consider previous and subsequent steps
-  Action:
-  \`\`\`
-  $JSON_BLOB
-  \`\`\`
-  Observation: action result
-  ... (repeat Thought/Action/Observation N times)
-  Thought: I know what to respond
-  Action:
-  \`\`\`
-  {
-    "{{TOOL_NAME_KEY}}": "Final Answer",
-    "{{ACTION_INPUT_KEY}}": "Final response to human"
-  }
-  \`\`\`
-
-  Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:\`\`\`$JSON_BLOB\`\`\`then Observation:.`,
-  completion: `
-  Respond to the human as helpfully and accurately as possible.
-
-{{instruction}}
-
-You have access to the following tools:
-
-{{tools}}
-
-Use a json blob to specify a tool by providing an {{TOOL_NAME_KEY}} key (tool name) and an {{ACTION_INPUT_KEY}} key (tool input).
-Valid "{{TOOL_NAME_KEY}}" values: "Final Answer" or {{tool_names}}
-
-Provide only ONE action per $JSON_BLOB, as shown:
-
-\`\`\`
-{{{{
-  "{{TOOL_NAME_KEY}}": $TOOL_NAME,
-  "{{ACTION_INPUT_KEY}}": $ACTION_INPUT
-}}}}
-\`\`\`
-
-Follow this format:
-
-Question: input question to answer
-Thought: consider previous and subsequent steps
-Action:
-\`\`\`
-$JSON_BLOB
-\`\`\`
-Observation: action result
-... (repeat Thought/Action/Observation N times)
-Thought: I know what to respond
-Action:
-\`\`\`
-{{{{
-  "{{TOOL_NAME_KEY}}": "Final Answer",
-  "{{ACTION_INPUT_KEY}}": "Final response to human"
-}}}}
-\`\`\`
-
-Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:\`\`\`$JSON_BLOB\`\`\`then Observation:.
-Question: {{query}}
-Thought: {{agent_scratchpad}}
-  `,
 }
 
 export const VAR_REGEX = /\{\{(#[a-zA-Z0-9_-]{1,50}(\.[a-zA-Z_][a-zA-Z0-9_]{0,29}){1,10}#)\}\}/gi
