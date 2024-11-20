2c00d923e4e3365e699a44e3e01b2846e65609bc
jescudero <jescudero@itos.es>
2024-11-20 13:52:58 +0100

    refactor: remove BlockInput component and related utility functions to streamline codebase

--- web/config/index.ts
+++ web/config/index.ts
@@ -33,12 +33,6 @@ export const SUPPORT_MAIL_LOGIN = !!(process.env.NEXT_PUBLIC_SUPPORT_MAIL_LOGIN 
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
-export const MAX_VAR_KEY_LENGTH = 30
-
-export const VAR_REGEX = /\{\{(#[a-zA-Z0-9_-]{1,50}(\.[a-zA-Z_][a-zA-Z0-9_]{0,29}){1,10}#)\}\}/gi
-
-export const resetReg = () => VAR_REGEX.lastIndex = 0
-
 export let textGenerationTimeoutMs = 60000
 
 if (process.env.NEXT_PUBLIC_TEXT_GENERATION_TIMEOUT_MS && process.env.NEXT_PUBLIC_TEXT_GENERATION_TIMEOUT_MS !== '')
